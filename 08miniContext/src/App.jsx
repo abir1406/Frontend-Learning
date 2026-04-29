d554eb4d75809282337bb81054eb876f1a948241
abir1406 <arkadeepchowdhury14@gmail.com>
2026-04-29 12:56:29 +0530

    feat: implement Login and Profile components with user context integration

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
+  
+
   return (
-    <div>
-      <h1>React with Chai</h1>
-    </div>
+    <UserContextProvider>
+      <h1>React with Chai </h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
